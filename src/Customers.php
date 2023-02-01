07e0a83dc7471588990ac06577160ce3c6e470ff
akmmp241 <akm.mp24@gmail.com>
2023-02-01 15:08:42 +0700

    add default value

--- src/Customers.php
+++ src/Customers.php
@@ -9,7 +9,7 @@ class Customers
     {
     }
 
-    public function sayHello(string $name): string
+    public function sayHello(string $name = "Guest"): string
     {
         return "Hello $name, my name is $this->name";
     }
